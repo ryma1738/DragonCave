f94a69b5d35a1be26bc5545756e85a9efcdbee42
Ryan Jepson <RyanJepson.webdev@gmail.com>
2022-01-29 15:59:30 -0700

    updating

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,7 +1,6 @@
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
@@ -9,10 +8,6 @@ public class AppTest {
     private String choice1 = "You enter the cave and see the dragon. The dragon tells you to come closer and take some of his treasure. "
                             +
             "'I have too much as it is'. You chose wisely and got the treasure. ";
-    @BeforeEach
-    void setUp () {
-       
-    } 
 
     @Test
     void choose() {
